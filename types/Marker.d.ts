d3a5a969f301e727ac0c6c6c0d9345f9373a568e
Serkan Okur <serkan.okur@gmail.com>
2024-08-13 23:51:54 +0200

    definitions for marker in d.ts files

--- types/Marker.d.ts
+++ types/Marker.d.ts
@@ -2,9 +2,12 @@ import { type EventedProps } from '@react-leaflet/core';
 import { Icon } from 'leaflet';
 import { type LatLngExpression, Marker as LeafletMarker, type MarkerOptions } from 'leaflet';
 import type { ReactNode } from 'react';
-export interface MarkerProps extends MarkerOptions, EventedProps {
+
+
+declare module 'leaflet' {
+  export interface MarkerOptions {
     children?: ReactNode;
     position: LatLngExpression;
     icon?: Icon;
+  }
 }
-export declare const Marker: import("react").ForwardRefExoticComponent<MarkerProps & import("react").RefAttributes<LeafletMarker<any>>>;
