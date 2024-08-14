e9c92f667d9c8c8904d45ed6e2f69e4d72839d70
Serkan Okur <serkan.okur@gmail.com>
2024-08-14 06:47:22 +0200

    fixing page widths for mobile after removing container for mobile

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const PrivacyPolicyPage = () => {
     return (
-        <main className="w-full md:w-11/12 max-w-7xl mx-auto px-4 ">
+        <main className="w-11/12 max-w-7xl mx-auto px-4 ">
             <PrivacyPolicy />
         </main>
     )
